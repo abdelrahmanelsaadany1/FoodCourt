300aa87dd844958f0ab7c4cf7afa4cba51e9f9a6
Abdelrahman Elsaadany <abdelrahmanelsaadany18@gmail.com>
2025-07-15 23:45:22 +0300

    Add comment changes

--- Core/Domain/Entities/Category.cs
+++ Core/Domain/Entities/Category.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
@@ -10,7 +11,8 @@ namespace Domain.Entities
     {
      
         public string Name { get; set; }
-    
+
+        [JsonIgnore]
 
         public virtual ICollection<Item> Items { get; set; }
     }

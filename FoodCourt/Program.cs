171fa450aa61ce6c475bc7b82040b92b097246fc
Abdelrahman Elsaadany <abdelrahmanelsaadany18@gmail.com>
2025-07-17 02:10:53 +0300

    Roles added and tested

--- FoodCourt/Program.cs
+++ FoodCourt/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Contracts;
 using Domain.Entities;
+using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 using Services.Abstractions.ICategoryService;
@@ -9,48 +11,53 @@ namespace FoodCourt
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Add DbContexts
             builder.Services.AddDbContext<FoodCourtDbContext>((optionsbuilder =>
             {
                 optionsbuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
             }));
+
             builder.Services.AddDbContext<IdentityContext>((optionsbuilder =>
             {
                 optionsbuilder.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
-
             }));
 
+            // Add Identity
+            builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            // Add your services
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             builder.Services.AddScoped<ICategoryService, CategoryService>();
-
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 
             var app = builder.Build();
 
+            // Initialize database and roles
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                await dbInitializer.InitializerIdentityAsync();
+            }
+
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
             app.UseSwagger();
             app.UseSwaggerUI();
-
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapGet("/", () => "✅ FoodCourt API — VERSION 1.3 ✅");
+            app.MapGet("/", () => "✅ FoodCourt API — VERSION 1.4 ✅");
             app.MapControllers();
 
             app.Run();

d10347ace8f877b87ceb0ebe32570eb726ca8afc
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-07-18 15:06:39 +0300

    Edit Address

---
+++ Infrastructure/Persistence/Migrations/Identity/20250718114123_FixAddressTableName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class FixAddressTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
